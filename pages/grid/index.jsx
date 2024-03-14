b57b5b35460f811ce313a912460766acffd80053
multicam <jmgiorgi@gmail.com>
2024-03-14 19:51:14 +1100

    -- responsive

--- pages/grid/index.jsx
+++ pages/grid/index.jsx
@@ -16,7 +16,7 @@ const GridPage = (props) => {
             <div className='grid grid-cols-12 gap-2'>
                 {data.dev.map((i, n) => {
                     return <div key={i} className={`grid-item col-span-4 overflow-hidden aspect-square flex justify-center items-center`}>
-                        <Image  className='min-h-full w-auto' src={'/images/dev/'+i+'.png'} width={1920} height={1281} sizes="(min-width: 768px) 720px, 85vw"/>
+                        <Image  alt={i.replace(/-/g, ' ')} width={1920} height={1260} className='min-h-full w-auto' src={'/images/dev/'+i+'.png'} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
                     </div>
                 })}
             </div>
