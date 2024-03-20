24f780cab4dd730605841af4572319cc67953cc2
multicam <jmgiorgi@gmail.com>
2024-03-21 10:22:31 +1100

    -- responsive 3

--- pages/slide/index.jsx
+++ pages/slide/index.jsx
@@ -1,4 +1,16 @@
 import Image from 'next/image'
+import Flickity from "react-flickity-component";
+
+import "flickity/css/flickity.css";
+
+function Carousel() {
+    return (
+        <Flickity>
+            <Image width={1200}  height={940} style={{ marginLeft: 5, marginRight: 5 }} src="/images/dev/orange-horn-mod-10-hd.png" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
+            <Image width={1200}  height={940} style={{ marginLeft: 5, marginRight: 5 }} src="/images/dev/pink-megaphone.png" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
+        </Flickity>
+    );
+}
 import Link from 'next/link'
 
 import data from './index.yaml'
@@ -9,16 +21,10 @@ const SlidePage = (props) => {
     return (<>
         <div>
             <Link className="p-2 text-xl font-bold" href={'/'}>Home</Link>
-            <Link className="p-2 text-xl font-bold" href={'/slides'}>Slides</Link>
+            <Link className="p-2 text-xl font-bold" href={'/grid'}>Grid</Link>
         </div>
-        <div className='px-8 py-4'>
-            <div className='grid grid-cols-12 gap-4'>
-                {data.images.map((i, n) => {
-                    return <div key={i} className={`grid-item col-span-4 overflow-hidden aspect-square flex justify-center items-center`}>
-                        <Image  width={1920} height={1260} className='min-h-full w-auto' src={'/images/dev/'+i+'.png'} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt={i.replace(/-/g, ' ')}/>
-                    </div>
-                })}
-            </div>
+        <div className='px-8 py-4 h-32'>
+            <Carousel />
         </div>
     </>)
 }
